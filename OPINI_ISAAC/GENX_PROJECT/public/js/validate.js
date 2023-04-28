c55204878128eabff3b2e2315dcc90bae088f541
opini isaac <isaacopini8@gmail.com>
2023-04-28 11:59:38 +0300

    html done

---
+++ OPINI_ISAAC/GENX_PROJECT/public/js/validate.js
@@ -0,0 +1,14 @@
+const Validate =()=>{
+    const firstName = document.getElementById('firstName')
+    const lastName = document.getElementById('lastName')
+    const dob = document.getElementById('dob')
+    const country = document.getElementById('country')
+    const state = document.getElementById('state')
+    const town = document.getElementById('town')
+    const zipCode = document.getElementById('zipCode')
+    const phoneNumber1 = document.getElementById('phoneNumber1')
+    const phoneNumber2 = document.getElementById('phoneNumber2')
+    const email = document.getElementById('email')
+i
+
+}
